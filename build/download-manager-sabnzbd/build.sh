1a3cc08fb33221984f35dc6240b7a7c54ee205f2
sjorge <sjorge@omnios.blackdot.be>
2013-07-10 16:32:02 +0200

    sabnzbd package

---
+++ build/download-manager-sabnzbd/build.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2013 Jorge Schrauwen.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../myfunc.sh
+
+PROG=SABnzbd                                 # App name
+VER=0.7.14                                   # App version
+VERHUMAN=$VER                                # Human-readable version
+#PVER=                                       # Branch (set in config.sh, override here if needed)
+PKG=network/download-manager/sabnzbd         # Package name (e.g. library/foo)
+SUMMARY="shared bit for download-manager range of packages."
+DESC="Shared bits for the download-manager range of packages."
+DEPENDS_IPS=""
+
+PREFIX=${PREFIX}-apps
+
+# Nothing to configure or build, just package
+download_source () {
+    cleanup_source
+
+    # fetch source
+    mkdir ${TMPDIR}/src
+
+    logmsg "--- download source"
+    wget -c http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/0.7.14/${PROG}-${VER}-src.tar.gz -O ${TMPDIR}/src/${PROG}-${VER}.tar.gz
+
+    # expand source
+    logmsg "--- unpacking source"
+    tar xzf ${TMPDIR}/src/${PROG}-${VER}.tar.gz -C ${TMPDIR}/src/
+
+}
+
+make_install() {
+    logmsg "--- make install"
+    logcmd mkdir -p ${DESTDIR}${PREFIX}/ || \
+        logerr "------ Failed to create destination directory."
+    logcmd mkdir -p ${DESTDIR}${PREFIX}/dlmgr/sabnzbd || \
+        logerr "------ Failed to create app directory."
+    logcmd mkdir -p ${DESTDIR}${PREFIX}/dlmgr/.config/sabnzbd || \
+        logerr "------ Failed to create app config directory."
+    logcmd mkdir -p $DESTDIR/lib/svc/manifest/network || \
+        logerr "------ Failed to create manifest directory."
+
+    logcmd cp -r ${TMPDIR}/src/${PROG}-${VER}/* ${DESTDIR}${PREFIX}/dlmgr/sabnzbd/ || \
+        logerr "------ Failed to copy app."
+    logcmd cp -r ${SRCDIR}/files/sabnzbd.xml $DESTDIR/lib/svc/manifest/network/ || \
+        logerr "------ Failed to copy manifest."
+}
+
+init
+prep_build
+download_source
+make_install
+make_package
+clean_up
+cleanup_source
+
+# Vim hints
+# vim:ts=4:sw=4:et:
