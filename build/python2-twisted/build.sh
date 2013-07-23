44b3d77f189845d19d0b107d583ec0eb668e30ee
sjorge <sjorge@omnios.blackdot.be>
2013-07-23 18:15:48 +0200

    python dependancies for deluge (partial)

---
+++ build/python2-twisted/build.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License"). You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc. All rights reserved.
+# Copyright 2013 Jorge Schrauwen.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../myfunc.sh
+
+PROG=Twisted
+VER=13.1.0
+PKG=library/python-2/twisted
+SUMMARY="An asynchronous networking framework written in Python."
+DESC="$SUMMARY"
+
+RUN_DEPENDS_IPS="runtime/python-26"
+BUILD_DEPENDS_IPS="runtime/python-26"
+
+# fix some compile issues
+export CFLAGS="-D_XOPEN_SOURCE=1 -D_XOPEN_SOURCE_EXTENDED=1"
+
+download_source() {
+    logmsg "Downloading Source"
+
+    cd ${TMPDIR}
+    wget -c https://pypi.python.org/packages/source/${PROG:0:1}/${PROG}/${PROG}-${VER}.tar.bz2
+
+    tar xvf ${PROG}-${VER}.tar.bz2
+}
+
+init
+auto_publish_wipe
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+python_build
+make_package
+auto_publish
+clean_up
