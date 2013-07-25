40260370b3cf16e8c579a94333b3d0d535c8f73b
sjorge <sjorge@omnios.blackdot.be>
2013-07-25 21:42:49 +0200

    mtr package

---
+++ build/network-mtr/build.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2013 Jorge Schrauwen.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../myfunc.sh
+
+PROG=mtr                                    # App name
+VER=0.85                                    # App version
+VERHUMAN=$VER-1                             # Human-readable version
+#PVER=                                      # Branch (set in config.sh, override here if needed)
+PKG=obd/network/mtr                         # Package name (e.g. library/foo)
+SUMMARY="Combines traceroute and ping into a single network diagnostic tool."
+DESC="${SUMMARY}"
+
+RUN_DEPENDS_IPS=""
+BUILD_DEPENDS_IPS=""
+
+BUILDARCH=both
+
+# package specific
+MIRROR=ftp.bitwizard.nl
+DLPATH=mtr
+
+download_source() {
+    logmsg "Downloading Source"
+
+    cd ${TMPDIR}
+    wget -c ftp://${MIRROR}/${DLPATH}/${PROG}-${VER}.tar.gz
+
+    tar xvf ${PROG}-${VER}.tar.gz
+}
+
+init
+auto_publish_wipe
+prep_build
+download_source ${DLPATH} ${PROG} ${VER}
+patch_source
+build
+make_isa_stub
+prefix_updater
+make_package
+auto_publish
+cleanup_source
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
