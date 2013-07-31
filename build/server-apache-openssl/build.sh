4f825336949bc3573b46bb5f92ea1a46a6eb9875
sjorge <sjorge@omnios.blackdot.be>
2013-08-01 01:13:21 +0200

    apache openssl

---
+++ build/server-apache-openssl/build.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2013 Jorge Schrauwen.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../myfunc.sh
+
+# main package config
+PROG=openssl                                 # App name
+VER=1.0.1e                                   # App version
+VERHUMAN=$VER-1                              # Human-readable version
+#PVER=                                       # Branch (set in config.sh, override here if needed)
+PKG=obd/server/apache/openssl                # Package name (e.g. library/foo)
+SUMMARY="Open Source toolkit implementing the Secure Sockets Layer (SSL v2/v3) and Transport Layer Security (TLS v1) protocols as well as a full-strength general purpose cryptography library managed by a worldwide community of volunteers that use the Internet to communicate, plan, and develop the OpenSSL toolkit and its related documentation."
+DESC="${SUMMARY}"
+
+RUN_DEPENDS_IPS="obd/server/apache/base"
+BUILD_DEPENDS_IPS="obd/server/apache/zlib"
+BUILDARCH=both
+
+PREFIX=${PREFIX}-apps/apache/shared
+
+# package specific
+MIRROR=www.openssl.org
+DLPATH=source
+
+# environment
+LDFLAGS32="-L${PREFIX}/lib -R${PREFIX}/lib"
+LDFLAGS64="-m64 -L${PREFIX}/lib/${ISAPART64} -R${PREFIX}/lib/${ISAPART64}"
+
+reset_configure_opts
+NO_PARALLEL_MAKE=1
+CONFIGURE_CMD="./Configure"
+CONFIGURE_OPTS="shared threads zlib-dynamic"
+CONFIGURE_OPTS_32="solaris-x86-gcc --prefix=${PREFIX} --with-zlib-lib=${PREFIX}/lib --with-zlib-include=${PREFIX}/include"
+CONFIGURE_OPTS_64="solaris64-x86_64-gcc --prefix=${PREFIX} --with-zlib-lib=${PREFIX}/lib/${ISAPART64} --with-zlib-include=${PREFIX}/include/${ISAPART64}"
+
+# override some functions
+make_install32() {
+    logmsg "--- make install"
+    logcmd $MAKE INSTALL_PREFIX=$DESTDIR LIBDIR=lib install ||
+        logerr "Failed to make install"
+
+    logmsg "--- fixing shitty build stuff"
+    logcmd mkdir -p  ${DESTDIR}${PREFIX}/bin/{i386,amd64} ||
+        logerr "Failed to create binary directories."
+    logcmd mv ${DESTDIR}${PREFIX}/bin/{openssl,c_rehash} ${DESTDIR}${PREFIX}/bin/${ISAPART}/ ||
+        logerr "Failed to move binaries."
+    logcmd tar cpf ${DESTDIR}${PREFIX}/include.tar include -C ${DESTDIR}${PREFIX} ||
+        logerr "Failed to store include headers."
+    logcmd rm -rf ${DESTDIR}${PREFIX}/include/* ||
+        logerr "Failed to remove include headers."
+}
+make_install64() {
+    logmsg "--- make install"
+    logcmd $MAKE INSTALL_PREFIX=$DESTDIR LIBDIR=lib/amd64 install ||
+        logerr "Failed to make install"
+
+    logmsg "--- fixing shitty build stuff"
+    logcmd mv ${DESTDIR}${PREFIX}/bin/{openssl,c_rehash} ${DESTDIR}${PREFIX}/bin/${ISAPART64}/ ||
+        logerr "Failed to move binaries."
+    logcmd mv ${DESTDIR}${PREFIX}/include ${DESTDIR}${PREFIX}/include_amd64 ||
+        logerr "Failed to move amd64 headers."
+    logcmd tar xpf ${DESTDIR}${PREFIX}/include.tar -C ${DESTDIR}${PREFIX} ||
+        logerr "Failed to restore i386 headers."
+    logcmd mv ${DESTDIR}${PREFIX}/include_amd64 ${DESTDIR}${PREFIX}/include/amd64 ||
+        logerr "Failed to restore amd64 headers."
+    logcmd rm ${DESTDIR}${PREFIX}/include.tar ||
+        logerr "Failed to cleanup header tarball."
+}
+
+init
+prep_build
+download_source ${DLPATH} ${PROG} ${VER}
+patch_source
+build
+make_isa_stub
+PREFIX=$(echo ${PREFIX} | sed "s#/shared##g")
+prefix_updater
+make_package
+auto_publish
+cleanup_source
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
