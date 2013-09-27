f080cf6b9c934381ac9a839d7242067619e74577
sjorge <sjorge@omnios.blackdot.be>
2013-09-27 18:08:39 +0200

    paramiko and pycrypto

---
+++ build/python2-paramiko/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License"). You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc. All rights reserved.
+# Copyright 2013 Jorge Schrauwen.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../myfunc.sh
+
+PROG=paramiko
+VER=1.11.1
+PKG=library/python-2/paramiko
+SUMMARY="This is a library for making SSH2 connections (client or server). Emphasis is on using SSH2 as an alternative to SSL for making secure connections between python scripts. All major ciphers and hash methods are supported. SFTP client and server mode are both supported too."
+DESC="$SUMMARY"
+
+RUN_DEPENDS_IPS="runtime/python-26 library/python-2/pycrypto"
+BUILD_DEPENDS_IPS="runtime/python-26"
+
+download_source() {
+    logmsg "Downloading Source"
+
+    cd ${TMPDIR}
+    wget -c https://pypi.python.org/packages/source/${PROG:0:1}/${PROG}/${PROG}-${VER}.tar.gz
+
+    tar xvf ${PROG}-${VER}.tar.gz
+}
+
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+python_build
+make_package
+auto_publish
+clean_up
